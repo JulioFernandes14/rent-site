931b622c83f1d923d3d37655255e58c28d4763c0
Julio Fernandes <juliocesarmf.1412@gmail.com>
2025-11-18 18:07:17 -0300

    fix: removendo comentário de codigo

--- app/_http/rent/interfaces.ts
+++ app/_http/rent/interfaces.ts
@@ -39,16 +39,3 @@ export interface RentCreateResponse {
     createdAt: string;
     items: RentItemCreateResponse[]
 }
-
-// {
-//   "id": "246e8dd9-f075-49a2-83b8-b9d5f1a99e6a",
-//   "createdAt": "2025-08-31T21:24:50.906Z",
-//   "items": [
-//     {
-//       "id": "1bf4b98b-0b25-41ae-835a-0a2c77ec44bf",
-//       "name": "Kit louça 1",
-//       "quantity": 5,
-//       "value": 25
-//     }
-//   ]
-// }
